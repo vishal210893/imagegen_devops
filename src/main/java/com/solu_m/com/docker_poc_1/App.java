21779978b9ddb5bb6ea9db4872dbd9e8d9d8d219
Vishal Kumar <vishal210893@gmail.com>
2024-10-24 23:18:59 +0530

    Update App.java

--- src/main/java/com/solu_m/com/docker_poc_1/App.java
+++ src/main/java/com/solu_m/com/docker_poc_1/App.java
@@ -4,13 +4,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.core.env.Environment;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @Slf4j
-@RestController
-@RequestMapping("api/v1")
 public class App {
 
     public static void main(String[] args) {
